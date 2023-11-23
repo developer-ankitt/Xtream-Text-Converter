1be0d263229e4b780c8c713ef3be4f703f448fd9
Ankit Kushwah <developer-ankitt@users.noreply.github.com>
2023-11-23 21:35:21 +0530

    In Navbar.js replace class - className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,38 +4,38 @@ const Footer = () => {
     return (
         <>
         {/* HEAD detached from d39b8d7 */}
-            <footer class="footer section bd-container">
-                <div class="footer__container bd-grid">
-                    <div class="footer__content">
-                        <a href="#" class="footer__logo">Xtream Text Converter</a>
-                        <span class="footer__description">Follow for more</span>
+            <footer className="footer section bd-container">
+                <div className="footer__container bd-grid">
+                    <div className="footer__content">
+                        <a href="#" className="footer__logo">Xtream Text Converter</a>
+                        <span className="footer__description">Follow for more</span>
                         <div>
-                            <a href="https://www.facebook.com/profile.php?id=100076434714683" target="_blank" class="footer__social"><i class='bx bxl-facebook'></i></a>
-                            <a href="https://www.instagram.com/yes_its_yummy.sandwich/" target="_blank" class="footer__social"><i class='bx bxl-instagram'></i></a>
+                            <a href="https://www.facebook.com/profile.php?id=100076434714683" target="_blank" className="footer__social"><i className='bx bxl-facebook'></i></a>
+                            <a href="https://www.instagram.com/yes_its_yummy.sandwich/" target="_blank" className="footer__social"><i className='bx bxl-instagram'></i></a>
                         </div>
                     </div>
 
-                    <div class="footer__content">
-                        <h3 class="footer__title">Services</h3>
+                    <div className="footer__content">
+                        <h3 className="footer__title">Services</h3>
                         <ul>
-                            <li><a href="#" class="footer__link">Uses</a></li>
-                            <li><a href="#" class="footer__link">Helpful links</a></li>
-                            <li><a href="#" class="footer__link">Support Us</a></li>
+                            <li><a href="#" className="footer__link">Uses</a></li>
+                            <li><a href="#" className="footer__link">Helpful links</a></li>
+                            <li><a href="#" className="footer__link">Support Us</a></li>
                         </ul>
                     </div>
 
-                    <div class="footer__content">
-                        <h3 class="footer__title">Information</h3>
+                    <div className="footer__content">
+                        <h3 className="footer__title">Information</h3>
                         <ul>
-                            <li><a href="#" class="footer__link">Event</a></li>
-                            <li><a href="#" class="footer__link">Contact us</a></li>
-                            <li><a href="#" class="footer__link">Privacy policy</a></li>
-                            <li><a href="#" class="footer__link">Terms of services</a></li>
+                            <li><a href="#" className="footer__link">Event</a></li>
+                            <li><a href="#" className="footer__link">Contact us</a></li>
+                            <li><a href="#" className="footer__link">Privacy policy</a></li>
+                            <li><a href="#" className="footer__link">Terms of services</a></li>
                         </ul>
                     </div>
 
-                    <div class="footer__content">
-                        <h3 class="footer__title">Address</h3>
+                    <div className="footer__content">
+                        <h3 className="footer__title">Address</h3>
                         <ul>
                             <li>60 Ft. Road</li>
                             <li>Near Axis Bank</li>
@@ -45,7 +45,7 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                {/* <p class="footer__copy">&#169; 2023 Yummy Sandwich. All right reserved <br><a href="https://instagram.com/developer_ankitt?igshid=OGQ5ZDc2ODk2ZA==" target="blank">Made with Love❤️@devloper_ankitt</a></p> */}
+                {/* <p className="footer__copy">&#169; 2023 Yummy Sandwich. All right reserved <br><a href="https://instagram.com/developer_ankitt?igshid=OGQ5ZDc2ODk2ZA==" target="blank">Made with Love❤️@devloper_ankitt</a></p> */}
             </footer>
         </>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,29 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <header class="l-header" id="header">
-            <nav class="nav bd-container">
-                <a href="#" class="nav__logo">Xtream Text Converter</a>
+        <header className="l-header" id="header">
+            <nav className="nav bd-container">
+                <a href="#" className="nav__logo">Xtream Text Converter</a>
 
-                <div class="nav__menu" id="nav-menu">
-                    <ul class="nav__list">
-                        <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
-                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
-                        <li class="nav__item"><a href="#services" class="nav__link">Services</a></li>
-                        <li class="nav__item"><a href="#menu" class="nav__link">Menu</a></li>
-                        <li class="nav__item"><a href="#contact" class="nav__link">Contact us</a></li>
+                <div className="nav__menu" id="nav-menu">
+                    <ul className="nav__list">
+                        <li className="nav__item"><a href="#home" className="nav__link active-link">Home</a></li>
+                        <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
+                        <li className="nav__item"><a href="#services" className="nav__link">Services</a></li>
+                        <li className="nav__item"><a href="#menu" className="nav__link">Menu</a></li>
+                        <li className="nav__item"><a href="#contact" className="nav__link">Contact us</a></li>
 
-                        <li><i class='bx bx-moon change-theme' id="theme-button"></i></li>
+                        <li><i className='bx bx-moon change-theme' id="theme-button"></i></li>
                     </ul>
                 </div>
 
-                <div class="nav__toggle" id="nav-toggle">
-                    <i class='bx bx-menu'></i>
+                <div className="nav__toggle" id="nav-toggle">
+                    <i className='bx bx-menu'></i>
                 </div>
             </nav>
         </header>
 
-        <main class="l-main"></main>
+        <main className="l-main"></main>
     </>
   )
 }

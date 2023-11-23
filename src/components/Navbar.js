b1c48624570a3f1cab1d41f0cddb12677b32dde6
Ankit Kushwah <developer-ankitt@users.noreply.github.com>
2023-11-24 00:19:36 +0530

    successfully added Navbar from the bootstrap Now I'll clean React App Remove all the comments & extra links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <header className="l-header" id="header">
-            <nav className="nav bd-container">
-                <a href="#" className="nav__logo">Xtream Text Converter</a>
-
-                <div className="nav__menu" id="nav-menu">
-                    <ul className="nav__list">
-                        <li className="nav__item"><a href="#home" className="nav__link active-link">Home</a></li>
-                        <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
-                        <li className="nav__item"><a href="#services" className="nav__link">Services</a></li>
-                        <li className="nav__item"><a href="#menu" className="nav__link">Menu</a></li>
-                        <li className="nav__item"><a href="#contact" className="nav__link">Contact us</a></li>
-
-                        <li><i className='bx bx-moon change-theme' id="theme-button"></i></li>
-                    </ul>
-                </div>
-
-                <div className="nav__toggle" id="nav-toggle">
-                    <i className='bx bx-menu'></i>
-                </div>
-            </nav>
-        </header>
-
-        <main className="l-main"></main>
+        
     </>
   )
 }

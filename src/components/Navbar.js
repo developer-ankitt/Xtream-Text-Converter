323a7c7962e7fcbddc885d4bf77a42e420f5bf7a
Ankit Kushwah <developer-ankitt@users.noreply.github.com>
2023-11-26 19:40:29 +0530

    Video #8 Completed ( Successfully Added:- No. of words | Char. | Read Time | Preview)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,31 +6,30 @@ const Navbar = (props) => {
     // export default function Navbar(props) {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">{props.title}</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">{props.title}</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">{props.home}</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">{props.about}</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">{props.about}</a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">{props.contact}</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">{props.contact}</a>
                             </li>
                         </ul>
 
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,8 +2,14 @@ import React, {useState} from 'react'
 
 const TextForm = (props) => {
     const [text, setText] = useState ('Enter text here...')
+
     const handelUpClick =()=>{
         let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handelLoClick =()=>{
+        let newText = text.toLowerCase();
         setText(newText)
     }
 
@@ -13,11 +19,22 @@ const TextForm = (props) => {
 
     return (
         <>
-            <div className='container mb-3'>
+            <div className='container my-3'>
             <h2>{props.heading}</h2>
+            <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handelOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-                <button className="btn btn-primary" onClick={handelUpClick} >Convert to Uppercase</button>
-                {/* <button className="btn btn-danger">Clear</button> */}
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handelUpClick} >Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handelLoClick} >Lowerercase</button>
+                <button className="btn btn-danger mx-2">Clear</button>
+            </div>
+
+            <div className="container my-3">
+                <h2>Here is the summary</h2>
+                <p>{text.split(" ").length} words & {text.length} Characters</p>
+                <p>{0.008*text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
             </div>
         </>
     )
